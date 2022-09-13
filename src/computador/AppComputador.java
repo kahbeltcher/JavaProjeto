439eb15f63e461af23ed584222e22f002df23c9d
kahbeltcher <81941451+kahbeltcher@users.noreply.github.com>
2022-09-13 08:39:48 -0300

    update

--- src/computador/AppComputador.java
+++ src/computador/AppComputador.java
@@ -1,40 +1,101 @@
 package computador;
 
+import java.util.Scanner;
+
 /**
  *
- * @author karen prazeres da silva 
- * matricula:20211303170 
- * aula: Programação orientada a objeto.
+ * @author kah
  */
-public class AppComputador{
+public class Computador {
 
-    public static void main(String[] args) {
+    //Instanciando scanner
+    Scanner sc = new Scanner(System.in);
+    //Atributos
+    public int armazenamento,memoria;
+    public String sistema,processador,marca;
 
-        Computador pc1 = new Computador();
-        Computador pc2 = new Computador();
-        Computador pc3 = new Computador();
-        Computador pc4 = new Computador();
+    //Construtores
+    public Computador(){
         
-        pc1.imprimir();
-        
-        System.out.println("----------------------------");
-        pc2.imprimir();
-        
-        System.out.println("----------------------------");
-        
-        pc3.entradaDados();
-        
-        System.out.println("----------------------------");
-        
-        pc3.imprimir();
-        
-        System.out.println("----------------------------");
-        
-        pc4.entradaDados();
-        
-        System.out.println("----------------------------");
-        
-        pc4.imprimir();
-        System.out.println("----------------------------");
+    }
+
+    public Computador(int armazenamento, int memoria, String sistema, String processador, String marca) {
+        this.armazenamento = armazenamento;
+        this.memoria = memoria;
+        this.sistema = sistema;
+        this.processador = processador;
+        this.marca = marca;
+    }
+
+    //SET 
+    public void setArmazenamento(int armazenamento) {
+        this.armazenamento = armazenamento;
+    }
+
+    public void setMemoria(int memoria) {
+        this.memoria = memoria;
+    }
+
+    public void setSistema(String sistema) {
+        this.sistema = sistema;
+    }
+
+    public void setProcessador(String processador) {
+        this.processador = processador;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    //GET 
+    public int getArmazenamento() {
+        return armazenamento;
+    }
+
+    public int getMemoria() {
+        return memoria;
+    }
+
+    public String getSistema() {
+        return sistema;
+    }
+
+    public String getProcessador() {
+        return processador;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    //ENTRADA DE DADOS
+    public void entradaDados() {
+        //ARMAZENAMENTO
+        System.out.print("Insira a quantidade de armazenamento do computador: ");
+        armazenamento = Integer.parseInt(sc.nextLine());
+        //MEMORIA
+        System.out.print("Insira a quantidade de memoria do computador: ");
+        memoria = Integer.parseInt(sc.nextLine());
+        //SISTEMA
+        System.out.print("Insira o sistema operacional do computador: ");
+        sistema = sc.nextLine();
+        //PROCESSADOR
+        System.out.print("Insira o processador do computador: ");
+        processador = sc.nextLine();
+        //MARCA
+        System.out.print("Insira a Marca do computador: ");
+        marca = sc.nextLine();
+    }
+
+    //SAIDA DE DADOS
+    public void imprimir() {
+        System.out.println(
+        "\nArmazenamento: " + armazenamento + "GB"
+        + "\nMemoria: " + memoria + " GB"
+        + "\nSistema: " + sistema
+        + "\nProcessador: " + processador
+        + "\nMarca: " + marca
+        );
     }
 }
